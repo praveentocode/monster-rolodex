ba7c73f9df256086fac4f5c254bd14795a31d919
Praveen Chandramohan <praveennin@gmail.com>
2020-08-24 14:50:11 +0530

    Updates

---
+++ precache-manifest.3d6947bcd562bc83abc478af3b685fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2700d422a9da2e52ccb388e31d555728",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2dddd146f396e012b17c",
+    "url": "/monster-rolodex/static/css/main.ec3fe3f3.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dddd146f396e012b17c",
+    "url": "/monster-rolodex/static/js/main.cbff520e.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
